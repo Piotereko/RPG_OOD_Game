9527a62404a098279291e59b33d11a59b13595c7
Pitu_PC <piotrczechowski14@gmail.com>
2025-03-04 17:06:17 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPG_wiedzmin_wanna_be
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
